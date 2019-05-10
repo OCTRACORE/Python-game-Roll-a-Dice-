b4dc0e24b5f2adf26e2d18a5e43d9acb0a9bc204
Jyotishka Aich <43207029+OCTRACORE@users.noreply.github.com>
2019-05-10 05:09:36 -0700

    Add files via upload

---
+++ d.py
@@ -0,0 +1,12 @@
+def to_camel_case(text):
+    titled = text.title()
+    words = titled.split()
+    camel_cased = " "
+    for word in words:
+      if word != ' ' or word != '-' or word != '_':
+        camel_cased += word
+
+    return camel_cased
+
+print(to_camel_case("hello-world"))
+
