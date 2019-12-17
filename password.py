e25d3c6f163036f6869d243149a8d8efcb064ff9
Jyotishka Aich <43207029+OCTRACORE@users.noreply.github.com>
2019-12-17 19:53:36 +0530

    Update password.py

--- password.py
+++ password.py
@@ -2,7 +2,7 @@ class password_manager():
     def __init__(self, n_,strength):
         self.strength = strength
         self.n_ = n_
-    def input_(self):
+    def input_r(self):
         self.n_ = int(input("number of characters: "))
         self.strength =  input("Strength: ")
-password_manager.input_(self)
+password_manager.input_r()
