8e84581675f6950d5c6018d53db2bb882a6d3311
Jyotishka Aich <43207029+OCTRACORE@users.noreply.github.com>
2019-12-19 20:54:09 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,49 +1,60 @@
 import random
-class password_creator():
-    def __init__(self,n,total):
-        self.total =  total
+class password_creator(): 
+    def __init__(self,n,jumbled_output,final_output): #making the local variable
         self.n  = n
-    def alpha_rand_func(self):
+        self.jumbled_output = jumbled_output
+        self.final_output = final_output
+    def alpha_rand_func(self): # for alphabetical part of the password
         alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        alpha_pass = ""
-        for i in range(0,(self.n//2)):
+        alpha_pass = "" # stores alphabetical part of the variable
+        for i in range(0,(self.n//2)): #for inserting random alphabets
             alpha_pass +=  random.choice(alpha)
+        
+        for x in range(self.n // 2):
+            self.jumbled_output += random.choice(alpha_pass) #for inserting random alphabets shortlisted previously in jumbled_output
         self.n -= self.n // 2
-        self.total += alpha_pass
         #print(alpha_pass)
-    def int_rand_func(self):
+
+    def int_rand_func(self):  #for numerical part of the password
         int_ = str(1234567890) 
-        int_pass = ""
+        int_pass = "" #stores numerical part of the variable
         for i in range(0,self.n):       
-            int_pass  += random.choice(int_)
-        self.total += int_pass
+            int_pass  += random.choice(int_) #inserting random integers for the
+        for x in range(self.n): #for inserting random numbers shortlisted previously in jumbled_output
+            self.jumbled_output += random.choice(int_pass)        
         #print(int_pass)
     def symbol_rand_func(self):
-        symbol = " ,./;'\[]-=`<>?:|{ }_+~"
-        sym_pass = ""
+        symbol = ",./;'\[]-=`<>?:|{ }_+~" 
+        sym_pass = "" #for storing symbolic prt of the password
         for i in range(0,5):
-            sym_pass += random.choice(symbol)
+            sym_pass += random.choice(symbol) #inserting random symbols in sym_pass
+        for i in range(5):
+            self.jumbled_output += random.choice(sym_pass) #for inserting random symbols shortlisted previously in jumbled_output
         self.n -= 5
-        self.total += sym_pass
+
         #print(sym_pass)
     def jumbling_func(self):
-        length  = len(self.total)
-        jumbled_output = ""
-        for x in self.total:
-            jumbled_output += random.choice(self.total)
-        print("Password: ",jumbled_output)
+       for i in self.jumbled_output:
+           self.final_output += random.choice(self.jumbled_output)
+       print(self.final_output)
+     
+    def clear(self): # for clearing the final_output and jumbled_output
+        self.final_output = ""
+        self.jumbled_output = ""
         
-
-def inp_():
-   number_of_sug = int(input("How many password do you want to make: "))
-   inp_ut = password_creator(int(input("No. of characters: ")),"")
-   for x in range(0,number_of_sug):  
     
-    inp_ut.symbol_rand_func()
-    inp_ut.alpha_rand_func()
-    inp_ut.int_rand_func()
-    inp_ut.jumbling_func()
-   return " "
-
-    
-print(inp_())
+def inp_ut(): # for user input
+  suggestions = int(input("no. of suggestions: ")) # for the no. of suggestions of password the user wants
+  inp_ = password_creator(int(input("no. of characters: ")),"","")
+  while inp_.n <=5 : # no. of characters cannot be less or equal to 5
+   inp_ = password_creator(int(input("no. of characters: ")),"","")
+  else:
+    for x in range(suggestions): #running the methods the desired number of times
+  
+      inp_.symbol_rand_func()
+      inp_.alpha_rand_func()
+      inp_.int_rand_func()
+      inp_.jumbling_func()
+      inp_.clear()
+  return " "   
+print(inp_ut())
