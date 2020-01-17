b68691675b00114407c0bc05f62dd25ece0ccb27
Jyotishka Aich <43207029+OCTRACORE@users.noreply.github.com>
2020-01-17 17:41:43 +0530

    Update password.py

--- password.py
+++ password.py
@@ -2,10 +2,11 @@ from random import *
 from itertools import *
 import string
 class password_generator():  
-   def __init__(self,final_output,special_nproduction,final_specp): #creating thwe local variables
+   def __init__(self,final_output,special_nproduction,final_specp): #creating the local variables
        self.final_output = final_output
        self.special_nproduction = special_nproduction
        self.final_specp = final_specp
+      
       
    def alpha_rand_func(self): #for randomly selecting alphabets
        alpha = string.ascii_letters
@@ -28,26 +29,23 @@ class password_generator():
           selected_comb_init_list = choice(init_perm) #selecting a combination of integers and symbols randomly
           mod_selected_comb_init = "" #for storing the combination in the form of a variable
           for i in selected_comb_init_list: #iterating through the selected_comb_init_list
-             mod_selected_comb_init += i
-          self.final_specp += mod_selected_comb_init 
+             mod_selected_comb_init += i 
+          self.final_output += mod_selected_comb_init  # adding the integer-symbol combination to final output
    def final_output_production(self): #for concatenating all the characters, selecting a combination of it randommly and returning it as output
-      self.final_output += self.final_specp
-      self.final_output = list(self.final_output)
-      x = list(permutations((self.final_output),8)) #creating arrangements of the combination
-      selected_comb = choice(x) #selecting a combination randomly
-      mod_selected_comb = "" #for storing the combination in the form of string
-      for i in selected_comb: #iterating through selected_comb
-         mod_selected_comb += i
-      return mod_selected_comb #returning the final combination
+           return self.final_output #returns the final output
+          
    
 ######################################
-print("Here are the following passwords")
-######################################
-for x in range(1,6): # for creating 5 paswords
- v =  password_generator("","","") #for calling the class
- alpha_g = v.alpha_rand_func()                      #line 48-53; calling the class methods
- sym_g = v.symbol_rand_func()
- int_g = v.int_rand_func()
- spec_g = v.special_number_production()
- final_g = v.final_output_production()
- print( x,":",final_g) 
+n_inp = (input("No. of suggestions you want: "))
+while n_inp.isdigit() == False:
+  n_inp = input("Please enter an appropriate input: ")
+else:
+ for x in range(1, int(n_inp) + 1): # for creating 5 paswords
+
+   v =  password_generator("","","") #for calling the class
+   alpha_g = v.alpha_rand_func()                      #line 48-53; calling the class methods
+   sym_g = v.symbol_rand_func()
+   int_g = v.int_rand_func()
+   spec_g = v.special_number_production()
+   final_g = v.final_output_production()
+   print( "%d : %s" % (x,final_g)) 
